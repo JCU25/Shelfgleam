7136fb308a7f8aab5d0f28e8e7a4519e33c90fce
JCU25 <janellecc.uy@gmail.com>
2026-05-18 14:33:29 +0800

    build: initialized backend server

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use("/", require("./routes/index"));
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
