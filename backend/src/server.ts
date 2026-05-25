90427ed3e1df21c5a49406f00f0020f035b72d89
JCU25 <janellecc.uy@gmail.com>
2026-05-25 22:31:31 +0800

    feat(docs)!: initial setup for swagger documentation
    feat!: added dependencies

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,10 @@
 import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "../../swagger.json" with { type: "json" };
+// import swaggerDocument from "../../swagger.js" with { type: "json" };
 import dotenv from "dotenv";
 import express from "express";
 import http from "http";
 import routes from "./routes/index.js";
+import { swaggerSpec } from "../swagger.js";
 
 dotenv.config({
 	path: ".env",
@@ -14,13 +15,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use("/", routes);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api", routes);
 
 const server = http.createServer(app);
 
 server.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
 	console.log(`API documentation available at http://localhost:${port}/docs`);
-	console.log(`API endpoint: http://localhost:${port}/v1`);
+	console.log(`API endpoint: http://localhost:${port}/api/v1`);
 });
