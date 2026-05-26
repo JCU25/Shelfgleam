8cca4ab57732196d9537a490ecfefd7b094c5be4
JCU25 <janellecc.uy@gmail.com>
2026-05-26 16:59:24 +0800

    docs: added swagger documentation on create user API

--- backend/src/routes/v1/userRoutes.ts
+++ backend/src/routes/v1/userRoutes.ts
@@ -3,6 +3,34 @@ import userController from "../../controllers/userController.js";
 
 const router = Router();
 
+/**
+ * @swagger
+ * /api/v1/users/signup:
+ *   post:
+ *     summary: Create new user
+ *     tags: [User]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required: [username, email, password]
+ *             properties:
+ *               username:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               displayName:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User created successfully
+ *       500:
+ *         description: Internal server error
+ */
 router.post("/signup", userController.signup);
 router.post("/login", userController.login);
 router.post("/logout", userController.logout);
