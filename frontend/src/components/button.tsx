5db01e394ae3dea45df15c715fa7046ac2210255
JCU25 <janellecc.uy@gmail.com>
2026-06-04 01:32:21 +0800

    feat(component): added type to Button props, and improved button style

--- frontend/src/components/button.tsx
+++ frontend/src/components/button.tsx
@@ -4,9 +4,10 @@ import clsx from "clsx";
 type props = {
 	children: React.ReactNode;
 	onClick?: () => void;
-	isPrimary: boolean;
+	isPrimary?: boolean;
 	customStyle?: string;
 	disabled?: boolean;
+	type?: "submit" | "reset" | "button";
 };
 
 export default function Button({
@@ -15,20 +16,23 @@ export default function Button({
 	isPrimary = true,
 	customStyle,
 	disabled = false,
+	type,
 }: props): ReactElement {
 	return (
 		<button
 			className={clsx(
-				`px-2 md:px-4 py-0 md:py-1 rounded-md`,
-				`disabled:bg-gray-300 disabled:text-gray-500`,
-				customStyle ?? customStyle,
+				`px-4 py-2 md:px-2 md:px-4 md:py-0 md:py-1 rounded-[10px]`,
+				`disabled:bg-purple-200 disabled:text-gray-500`,
 
 				isPrimary
-					? `bg-purple-600 border-purple-900  text-white hover:bg-purple-700 active:bg-purple-800`
-					: `bg-white border-purple-700 border text-black hover:bg-purple-100`,
+					? `bg-purple-600 border-purple-900  text-white hover:bg-purple-500 active:bg-purple-800`
+					: `bg-white border-purple-700 border text-black hover:bg-purple-100 active:bg-purple-400`,
+
+				customStyle,
 			)}
 			onClick={onClick}
 			disabled={disabled}
+			type={type}
 		>
 			{children}
 		</button>
