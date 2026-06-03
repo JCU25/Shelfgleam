61c5850a2ba0995b1a92437ba917851efdcb74a5
JCU25 <janellecc.uy@gmail.com>
2026-06-04 01:37:28 +0800

    feat: setup axios interceptor for non-authenticated APIs

---
+++ frontend/src/utlils/publicApi.tsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const api = axios.create({
+	baseURL: import.meta.env.VITE_API_ENTPOINT,
+});
+
+api.interceptors.response.use(
+	(response) => response,
+	(error) => {
+		return Promise.reject<string>(Error(error?.response?.data?.message));
+	},
+);
